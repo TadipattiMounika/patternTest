5a76fb70751c8be1d4b1c811544c4db494743223
TadipattiMounika <116668614+TadipattiMounika@users.noreply.github.com>
2022-10-26 22:44:38 +0530

    Add files via upload

---
+++ IneuronPattern.java
@@ -0,0 +1,101 @@
+
+public class IneuronPattern 
+{
+	public static void main(String[] args) 
+		{
+		    int n=11;
+		    System.out.println(" ");
+			for (int i=0;i<n;i++)
+			{
+			    for (int j=0;j<n;j++)
+			    {
+			        if((j==n/2) || i==0 || i==n-1 )
+			        {
+			        System.out.print("*");
+			        }
+			        else 
+			        {
+			        System.out.print(" ");
+			        }
+			    }
+			    System.out.print(" ");
+			    for (int j=0;j<n;j++)
+			    {
+			        if((j==0) || j==n-1 || i==j )
+			        {
+			        System.out.print("*");
+			        }
+			        else 
+			        {
+			        System.out.print(" ");
+			        }
+			    }
+			    System.out.print(" ");
+			    for (int j=0;j<n;j++)
+			    {
+			        if((j==0) || i==n-1 || i==0 || i==n/2 )
+			        {
+			        System.out.print("*");
+			        }
+			        else 
+			        {
+			        System.out.print(" ");
+			        }
+			    }
+			    System.out.print(" ");
+			    for (int j=0;j<n;j++)
+			    {
+			        if((j==0) && i<n-1 || j==n-1 && i<n-1 || i==n-1 && j>0 && j<n-1 )
+			        {
+			        System.out.print("*");
+			        }
+			        else 
+			        {
+			        System.out.print(" ");
+			        }
+			    }
+			    System.out.print(" ");
+			    for (int j=0;j<n;j++)
+			    {
+			        if((j==0) && i>0 || i==0 && j>0 && j<n/2 || j==n/2 && i>0 && i<n/2 || i-j==(n-1)/2 || i==n/2 && j<n/2)
+			        {
+			        System.out.print("*");
+			        }
+			        else 
+			        {
+			        System.out.print(" ");
+			        }
+			    }
+			    System.out.print(" ");
+			    for (int j=0;j<n;j++)
+			    {
+			        if((j==0) && i<n-1 && i>0 || j==n-1 && i<n-1 && i>0 || i==n-1 && j>0 && j<n-1 || i==0 && j>0 && j<n-1 )
+			        {
+			        System.out.print("*");
+			        }
+			        else 
+			        {
+			        System.out.print(" ");
+			        }
+			    }
+			     System.out.print(" ");
+			    for (int j=0;j<n;j++)
+			    {
+			        if((j==0) || j==n-1 || i==j )
+			        {
+			        System.out.print("*");
+			        }
+			        else 
+			        {
+			        System.out.print(" ");
+			        }
+			    }
+			    
+			    System.out.println("  ");
+			    
+			}
+		}
+	}
+
+
+
